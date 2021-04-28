25c2091b64b105f8a17da4ba514360a7f4c6bc5f
Kukks <evilkukka@gmail.com>
2021-04-28 12:11:49 +0200

    make script more warningish

--- Tools/toggle_bitcoin_taproot_bip8_lottrue..sh
+++ Tools/toggle_bitcoin_taproot_bip8_lottrue..sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ "$0" = "$BASH_SOURCE" ]]; then
-    echo "This script must be sourced \". toggle_bitcoin_taproot.sh\"" 
+    echo "This script must be sourced \". toggle_bitcoin_taproot_bip8_lottrue.sh\"" 
     exit 1
 fi
 
@@ -22,11 +22,11 @@ fi
 
 
 
-read -p "This script will swap the official Bitcoin release with an unofficial version provided from https://bitcointaproot.cc that includes taproot activation signalling. Type 'unofficial' to switch to this UNOFFICIAL FORK OF BITCOIN CORE. `echo $'\n> '`" yn
+read -p "This script will swap the official Bitcoin release with an unofficial version provided from https://bitcointaproot.cc that includes taproot activation signalling via BIP8 wiht LOT=TRUE. Type 'unofficial' to switch to this UNOFFICIAL FORK OF BITCOIN CORE. `echo $'\n> '`" yn
 if [ $yn != "unofficial" ]; then
 	exit 0
 fi
-read -p "OK, last chance to abort. Type 'yes' to continue! `echo $'\n> '`" yn
+read -p "OK, Are you sure you know what you are doing? Please be aware that you running a Bitcoin node that is not from the official release group and cycle. There can be a potential chain split in special circumstances. ONLY RUN THIS IF YOU ARE KNOWLEDGEABLE. BTCPAYSERVER DOES NOT PROVIDE AN OPINION ON THIS DECISION AND THIS OPTION DOES NOT MEAN WE ARE PROMOTING THIS FORK OF BITCOIN CORE BY LUKEDASHJR.  Type 'yes' to continue! `echo $'\n> '`" yn
 if [ $yn != "yes" ]; then
 	exit 0
 fi
